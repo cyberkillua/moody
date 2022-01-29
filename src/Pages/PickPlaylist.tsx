da25e30601b4431cf998ecb42e6016f19aa1a688
cyberkillua <moshood988@gmail.com>
2022-01-29 20:57:23 +0100

    refactor code

--- src/Pages/PickPlaylist.tsx
+++ src/Pages/PickPlaylist.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Footer from "../Component/Footer";
 import Header from "../Component/Header";
-import gsap from "gsap";
 
 export default function PickPlaylist() {
- 
-  // useEffect(() => {
-  //   gsap.to('.pick-playlist-container', 0, { css: { visibility: "visible" } });
-  // }, []);
-
   return (
-    <div  className="pick-playlist-container">
+    <div className="pick-playlist-container">
       <Header />
       <main className="container">
         <h1 className="container-header">Pick your preferred playlist</h1>
